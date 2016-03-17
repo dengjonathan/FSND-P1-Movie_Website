c534d90ecf3d3475a8c8dae68e879d6bbfc37d4c
dengjonathan <dengjonathan@users.noreply.github.com>
2016-03-17 13:10:01 -0400

    Update media.py
    To comply with PEP8 standards

--- media.py
+++ media.py
@@ -4,19 +4,17 @@ Created on Wed Mar 16 16:15:14 2016
 
 @author: jonathandeng
 """
+import webbrowser
 
-class Movie():  
-    '''stores movie attributes within an instance of class Movie'''
-    
+
+class Movie(object):
+    """stores movie attributes within an instance of class Movie"""
+
     def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
-        self.trailer_youtube_url= trailer_youtube_url
-        
+        self.trailer_youtube_url = trailer_youtube_url
+
     def show_trailer(self):
-        import webbrowser
         webbrowser.open(self.trailer)
-
-
-        
